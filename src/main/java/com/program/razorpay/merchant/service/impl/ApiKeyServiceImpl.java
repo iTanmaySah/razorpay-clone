3f44311e77fd3062c069b896f47f831626458d53
Tanmay Sah <tanmay.sah14@gmail.com>
2026-06-21 15:18:53 +0530

    added isEnabled check to apiKey rotation

--- src/main/java/com/program/razorpay/merchant/service/impl/ApiKeyServiceImpl.java
+++ src/main/java/com/program/razorpay/merchant/service/impl/ApiKeyServiceImpl.java
@@ -80,6 +80,9 @@ public class ApiKeyServiceImpl implements ApiKeyService {
                 .filter(k -> k.getMerchant().getId().equals(merchantId))
                 .orElseThrow(() -> new ResourceNotFoundException("ApiKey", keyId));
 
+        if(!apiKey.isEnabled())
+            throw new RuntimeException("Cannot rotate a disabled key");
+
         String newRawSecret = RandomizerUtil.randomBase64(40);
         apiKey.setPreviousKeySecretHash(apiKey.getKeySecretHash());
         apiKey.setKeySecretHash(newRawSecret);  // TODO: encode with BcryptPasswordEncoder
